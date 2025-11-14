909ff872837b673905c18d1e47e664c94c618ec1
Isnardo Aburto <138733050+IsnardoA@users.noreply.github.com>
2025-11-13 22:09:44 -0500

    codewars Basic Mathematical Operations 8kyu

--- main.js
+++ main.js
@@ -37,9 +37,31 @@
 //   }
 //   return sum
 // }
-function positiveSum(arr) {
-  return arr.reduce((a, b) => a + (b > 0 ? b : 0), 0)
-} 
+// function positiveSum(arr) {
+//   return arr.reduce((a, b) => a + (b > 0 ? b : 0), 0)
+// } 
+// console.log(positiveSum([1, 2, 3, 4, 5]))
 
-console.log(positiveSum([1, 2, 3, 4, 5]))
 
+// Your task is to create a function that does four basic mathematical operations.
+// The function should take three arguments - operation(string / char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+    case '+':
+      return value1 + value2;
+    case '-':
+      return value1 - value2;
+    case '*':
+      return value1 * value2;
+    case '/':
+      return value1 / value2;
+    default:
+      return 0;
+  }
+}
+
+// function basicOp(o, a, b) {
+//   return eval(a + o + b);
+// }
