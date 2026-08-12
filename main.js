af38667a9bf80c6eb7a8769b9dc51f4e8b81c2ab
Isnardo Aburto <138733050+IsnardoA@users.noreply.github.com>
2026-08-11 21:48:28 -0400

    New codewars problem solved

--- main.js
+++ main.js
@@ -227,3 +227,26 @@
 // 	});
 // }
 // console.log(filter_list([1, 2, "a", "b"]));
+
+// Array.dif
+// Implement a function that computes the difference between two lists. The function should remove all occurrences of elements from the first list (a) that are present in the second list (b). The order of elements in the first list should be preserved in the result.
+// Examples
+// If a = [1, 2] and b = [1], the result should be [2].
+// If a = [1, 2, 2, 2, 3] and b = [2], the result should be [1, 3].
+
+// function arrayDiff(a, b) {
+// 	return a.filter((x) => !b.includes(x));
+// }
+function arrayDiff(a, b) {
+	if (b.length == 0 || a.length == 0) return a;
+	for (let i = 0; i < a.length; i++) {
+		for (let j = 0; j < b.length; j++) {
+			if (a[i] == b[j]) {
+				a.splice(i, 1);
+				i--;
+			}
+		}
+	}
+	return a;
+}
+console.log(arrayDiff([1, 2], [1]));
